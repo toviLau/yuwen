7919c24c38e68e316c009c84bd383dc3fae6a69c
ToviLau <46134256@qq.com>
2023-09-26 05:35:23 +0800

    var

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <script>
 export default {
-  onLaunch: function () {
-    console.log('App Launch')
-  },
-  onShow: function () {
-    console.log('App Show')
-  },
-  onHide: function () {
-    console.log('App Hide')
-  },
+    onLaunch: function () {
+        console.log('App Launch')
+    },
+    onShow: function () {
+        console.log('App Show')
+    },
+    onHide: function () {
+        console.log('App Hide')
+    },
 }
 </script>
 
 <style>
-page{
-  display: flex;
-  height:100%;
+@import url('./module/css/base.less');
+page {
+    display: flex;
+    height: 100%;
 }
+
 /*每个页面公共css */
-uni-page-body{
-  height:100%;
-  display: flex;
-  width: 100%;
+uni-page-body {
+    height: 100%;
+    display: flex;
+    width: 100%;
 }
 </style>
